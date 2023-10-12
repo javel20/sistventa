54f93052fd116f87b458da54b17eeaab9589fed6
javier elias <javier_el_balla@hotmail.com>
2023-10-12 08:43:06 -0500

    code rutas

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+    //nombre del archivo donde se ejecuto el script
+    //dirname para extraer la primera parte del enlace
+    $folderPath = dirname($_SERVER['SCRIPT_NAME']);
+
+    //trae la ruta completa de la url, la que estamos ubicados
+    $urlPath = $_SERVER['REQUEST_URI'];
+
+    // $url = substr($urlPath,11); 
+    //substr muestra nombre apartir del numero(caracter) escrito
+    //strlen, longitud de cadena de texto de  folderpath
+    $url = substr($urlPath,strlen($folderPath));
+
+    //define constante global llamada url se almacena $url
+    define('URL',$url);
+
+    // echo '<PRE>';
+    // var_dump($_SERVER);
+    // echo '</PRE>';
+
+    // echo $folderPath .'<br>';
+    // echo $urlPath .'<br>';
+    // echo $url .'<br>';
+
+
+    
+?>
